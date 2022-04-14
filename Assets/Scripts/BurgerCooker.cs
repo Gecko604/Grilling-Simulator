c873d6bd164e239051a707f82a9faa5914dee72a
Jackson Herman <hermanjacksonw@gmail.com>
2022-04-14 15:50:31 -0500

    Deleted unused code in scripts

--- Assets/Scripts/BurgerCooker.cs
+++ Assets/Scripts/BurgerCooker.cs
@@ -9,8 +9,6 @@ public class BurgerCooker : MonoBehaviour
     public GameObject bottomBurger;
 
     private AudioSource grillingSound;
-    //public BurgerStager UpperStager;
-    //public BurgerStager LowerStager;
 
 
     // Start is called before the first frame update
@@ -20,7 +18,6 @@ public class BurgerCooker : MonoBehaviour
         if (bottomBurger.GetComponent<BurgerStager>() == null) { Debug.Log("Missing LowerStager"); };
 
         grillingSound = GetComponent<AudioSource>();
-        //Debug.Log("BurgerCooker.cs Start");
     }
 
     // Update is called once per frame
@@ -31,29 +28,23 @@ public class BurgerCooker : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        //Debug.Log($"OnTriggerEnter other: {other.gameObject}");
         if (other.transform.tag == "Grill")
         {
             print("playing sound");
             grillingSound.Play();
             if (GetHalf())
             {
-                //Debug.Log("Upper start grilling");
                 topBurger.GetComponent<BurgerStager>().StartGrilling();
-                //Debug.Log("Upper started grilling");
             }
             else
             {
-                //Debug.Log("Lower start grilling");
                 bottomBurger.GetComponent<BurgerStager>().StartGrilling();
-                //Debug.Log("Lower started grilling");
             }
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        //Debug.Log($"OnTriggerExit other: {other.gameObject}");
         if (other.transform.tag == "Grill")
         {
             print("stopping sound");
@@ -65,10 +56,6 @@ public class BurgerCooker : MonoBehaviour
 
     private bool GetHalf()
     {
-        //float upAngle = Quaternion.Angle(Quaternion.Euler(0, -90, 0), transform.rotation);
-        //float downAngle = Quaternion.Angle(Quaternion.Euler(0, 90, 0), transform.rotation);
-        //Debug.Log($"GetHalf() upAngle: {upAngle} downAngle: {downAngle}");
-
         if(topBurger.transform.position.y > bottomBurger.transform.position.y)
         {
             return true;
--- Assets/Scripts/BurgerStager.cs
+++ Assets/Scripts/BurgerStager.cs
@@ -19,16 +19,12 @@ public class BurgerStager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("Agony: Unity crashed");
-
         if (gameObject.GetComponent<MeshRenderer>().material == null) { Debug.Log("Missing MeshRenderer"); return; };
-        //gameObject.GetComponent<MeshRenderer>().material = Raw;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("life is pain, a stark reminder every frame of this godless project");
         if (heat >= 10f && heat < 20f)
             gameObject.GetComponent<MeshRenderer>().material = MediumRare;
         else if (heat >= 20f && heat < 30f)
@@ -44,13 +40,11 @@ public class BurgerStager : MonoBehaviour
 
     public void StartGrilling()
     {
-        //Debug.Log("endless torment, why must i be burnt");
         onGrill = true;
     }
 
     public void StopGrilling()
     {
-        //Debug.Log("i lament the time when I must burn again, please make it not so.");
         if (onGrill)
         {
             onGrill = false;
@@ -62,7 +56,6 @@ public class BurgerStager : MonoBehaviour
 
     public IEnumerator LatentHeat()
     {
-        //Debug.Log("Let the meat burning commence, Isaiah 12:32");
         float internalHeat = 0;
         while (internalHeat < 5f)
         {
@@ -72,6 +65,5 @@ public class BurgerStager : MonoBehaviour
             yield return new WaitForSeconds(1);
             internalHeat += 1;
         }
-        //Debug.Log("burg cool");
     }
 }

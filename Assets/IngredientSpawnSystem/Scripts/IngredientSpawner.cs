b371960f86a0d4ef1028fa0a22d0176551e11187
Jackson Herman <hermanjacksonw@gmail.com>
2022-04-19 16:08:57 -0500

    added ingredient spawner script

---
+++ Assets/IngredientSpawnSystem/Scripts/IngredientSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour
+{
+
+    public Vector3 startPos;
+    public GameObject ingredient;
+    public Quaternion startRot;
+
+    private bool canSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = gameObject.transform.position;
+        startRot = gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnIngredient()
+    {
+        if (canSpawn)
+        {
+            GameObject ingredientInstance = Instantiate(ingredient, startPos, startRot) as GameObject;
+            ingredientInstance.name = "Burger Patty";
+            ingredientInstance.GetComponent<Rigidbody>().isKinematic = false;
+            canSpawn = false;
+        }
+    }
+}

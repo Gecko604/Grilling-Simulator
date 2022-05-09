b1bc8b549a15c7b139ba6bf3dd6fbe95fb6a4875
Alexander Kupec <alex.kupec@comcast.net>
2022-05-09 16:41:18 -0500

    Added separate spatula for game quitting.

--- Assets/Kupec_Features/ResetGame.cs
+++ Assets/Kupec_Features/ResetGame.cs
@@ -16,10 +16,25 @@ public class ResetGame : MonoBehaviour
         StartCoroutine(resetGame());
     }
 
+    public void ExitGame()
+    {
+        StartCoroutine(exitGame());
+    }
+
     IEnumerator resetGame()
     {
+        Debug.Log("Restarting in 3 seconds..");
         yield return new WaitForSeconds(3);
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator exitGame()
+    {
+        Debug.Log("Exiting in 3 seconds..");
+        yield return new WaitForSeconds(3);
+
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
